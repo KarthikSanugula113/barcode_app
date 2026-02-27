370b3d27b2082132b9a473362a50d1d940c5180b
KarthikSanugula113 <karthiksanugula511@gmail.com>
2026-02-27 01:34:28 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,48 +1,32 @@
-from flask import Flask, request, send_file, render_template_string
-import barcode
-from barcode.writer import ImageWriter
-from io import BytesIO
-import random
+from flask import Flask
+from vercel_wsgi import handle_request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
-HTML = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Barcode App</title>
-</head>
-<body style="text-align:center; margin-top:50px;">
-    <h2>Flask Barcode App 🚀</h2>
-    <form method="POST">
-        <input type="number" step="0.01" name="price" placeholder="Enter price" required>
-        <br><br>
-        <button type="submit">Generate</button>
-    </form>
-</body>
-</html>
-"""
+def handler(request):
+    return handle_request(app, request)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         price = request.form.get("price")
 
-        euro_part = int(float(price))
-        price_block = str(euro_part).zfill(4)
-        random_part = str(random.randint(10**15, 10**16-1))
+        if price:
+            euro_part = int(float(price))
+            price_block = str(euro_part).zfill(4)
+            random_part = str(random.randint(10**15, 10**16-1))
 
-        final_string = "ADR" + price_block + random_part
+            final_string = "ADR" + price_block + random_part
 
-        CODE128 = barcode.get_barcode_class("code128")
-        my_barcode = CODE128(final_string, writer=ImageWriter())
+            CODE128 = barcode.get_barcode_class("code128")
+            my_barcode = CODE128(final_string, writer=ImageWriter())
 
-        buffer = BytesIO()
-        my_barcode.write(buffer)
-        buffer.seek(0)
+            buffer = BytesIO()
+            my_barcode.write(buffer)
+            buffer.seek(0)
 
-        return send_file(buffer, mimetype="image/png")
+            return send_file(buffer, mimetype="image/png")
 
-    return render_template_string(HTML)
+    return render_template("index.html")
 
-handler = app
+# Required for Vercel
